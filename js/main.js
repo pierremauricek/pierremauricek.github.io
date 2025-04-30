3b7a67cd04084585fcb81361be7b594663d71144
Pierre-Maurice K <37899679+pierremauricek@users.noreply.github.com>
2025-04-30 06:37:33 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-function spawnFooterImage(event) {
-    event.preventDefault(); // prevent link navigation, remove if not needed
-  
-    const img = document.createElement('img');
-    img.src = '/img/heart.png'; // your image URL here
-    img.className = 'spawned-footer-img ';
-  
-    // Position the image at the cursor
-    img.style.left = `${event.clientX}px`;
-    img.style.top = `${event.clientY}px`;
-  
-    document.body.appendChild(img);
-  
-    // Optional: fade out before removal
-    setTimeout(() => {
-      img.style.opacity = '0';
-      setTimeout(() => img.remove(), 300); // wait for fade-out
-    }, 1000); // remove after 1 second
-  }
-
-  
-// MENU TOGGLE
+ // MENU TOGGLE
 
   const toggleMenu = document.getElementById('menu-button');
   const menu = document.getElementById('menu');
