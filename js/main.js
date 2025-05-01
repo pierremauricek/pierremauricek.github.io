b51f8990456a7c34831b22924102c8b28ceb43f5
Pierre-Maurice K <37899679+pierremauricek@users.noreply.github.com>
2025-05-01 17:32:33 +0200

    update

--- js/main.js
+++ js/main.js
@@ -54,10 +54,8 @@ function updateTheme(e) {
   const newTheme = e.matches ? 'dark' : 'light';
   document.documentElement.setAttribute('data-theme', newTheme);
 }
-
 // Initial setzen
 updateTheme(mediaQuery);
-
 // Listener hinzufügen
 mediaQuery.addEventListener('change', updateTheme);
 
@@ -86,30 +84,7 @@ mediaQuery.addEventListener('change', updateTheme);
 
 // LIGHTBOX
 
-document.querySelectorAll('.lightbox-image').forEach(img => {
-  img.addEventListener('click', () => {
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImg = document.getElementById('lightbox-img');
-    lightboxImg.src = img.src;
 
-    lightbox.classList.toggle('show'); // Add the "show" class to trigger the animation
-    document.body.classList.toggle('lock-scroll'); // Prevent scrolling
-  });
-});
-
-document.querySelector('.close').addEventListener('click', () => {
-  const lightbox = document.getElementById('lightbox');
-  lightbox.classList.remove('show'); // Remove the "show" class to hide the lightbox
-  document.body.classList.remove('lock-scroll'); // Re-enable scrolling
-});
-
-window.addEventListener('click', (e) => {
-  const lightbox = document.getElementById('lightbox');
-  if (e.target === lightbox) {
-    lightbox.classList.remove('show'); // Remove the "show" class to hide the lightbox
-    document.body.classList.remove('lock-scroll'); // Re-enable scrolling
-  }
-});
 
 
 
