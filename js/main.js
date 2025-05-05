ca72577a44ffd4f4b03f08e4cbd4831e8391758a
Pierre-Maurice K <37899679+pierremauricek@users.noreply.github.com>
2025-05-05 17:07:41 +0200

    update

--- js/main.js
+++ js/main.js
@@ -81,38 +81,6 @@ mediaQuery.addEventListener('change', updateTheme);
  // });
 
 
-
-// LIGHTBOX
-
-document.querySelectorAll('.lightbox-image').forEach(img => {
-  img.addEventListener('click', () => {
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImg = document.getElementById('lightbox-img');
-    lightboxImg.src = img.src;
-
-    lightbox.classList.toggle('show'); // Add the "show" class to trigger the animation
-    document.body.classList.toggle('lock-scroll'); // Prevent scrolling
-  });
-});
-
-document.querySelector('.close').addEventListener('click', () => {
-  const lightbox = document.getElementById('lightbox');
-  lightbox.classList.remove('show'); // Remove the "show" class to hide the lightbox
-  document.body.classList.remove('lock-scroll'); // Re-enable scrolling
-});
-
-window.addEventListener('click', (e) => {
-  const lightbox = document.getElementById('lightbox');
-  if (e.target === lightbox) {
-    lightbox.classList.remove('show'); // Remove the "show" class to hide the lightbox
-    document.body.classList.remove('lock-scroll'); // Re-enable scrolling
-  }
-});
-
-
-
-
-
 // Easter Egg
 const khaos_img = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠶⢮⣧⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⣸⢟⠶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡪⢣⡜⡱⡱⣻⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⣿⢕⡳⢬⢌⠻⣢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢎⢥⢱⢳⢎⠷⡫⣻⠁⠀⠀⠀⠀\n⠀⠀⠀⠀⢹⡎⡲⣑⠕⣌⢦⠙⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⢼⠱⡣⣣⣙⡵⣛⡿⣌⢻⡂⠀⠀⠀⠀\n⠀⠀⠀⠀⢸⡇⡪⢔⡙⢦⢣⢝⡔⣝⢷⢄⣀⡀⡀⠀⠀⠀⠀⠀⡀⡀⠀⡀⠀⣀⡀⣠⠰⡢⣕⡕⣋⣎⡵⢥⣹⡣⢗⢌⡾⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠈⣷⢩⠮⡼⠴⡫⢞⡜⣵⢪⡳⣍⢝⢞⢮⡹⣄⠑⡥⢈⡆⢱⢈⣜⢵⢜⢭⡎⡳⢵⡭⢵⡭⡫⠺⣧⡛⡵⣑⠇⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠹⡪⣪⢳⣟⢏⣟⢼⡪⣗⠳⢵⡝⡮⣳⣛⡌⢒⢊⠆⣗⢱⡄⡹⣪⠗⡾⣕⡱⡺⣜⡳⠼⣝⣞⡵⣫⢎⡾⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⢱⢕⠧⣝⣭⢵⢝⢮⠕⡯⡊⡧⠨⡎⡽⣐⠣⡙⣪⢧⡘⡆⢆⣜⠗⡼⡄⣍⠺⡠⡞⢔⠜⡥⠻⡮⡺⡅⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⡣⢳⡔⡗⡑⠣⡃⠞⢕⠔⢄⢕⣻⠠⣻⣆⢏⡪⡾⢼⡂⡷⣝⠜⡵⡬⡂⢜⠕⢜⠄⠣⡨⢞⡕⣾⠂⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢢⢄⡩⡨⡈⠆⢩⠘⠄⠣⠈⢦⣻⠰⡱⣷⠱⣏⢗⣫⡳⣹⣝⡜⣹⢞⢔⢐⠉⡂⠑⢌⠂⢂⠕⢻⣧⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢸⡗⠪⡐⡈⢂⠅⡈⢄⢊⢄⠙⢮⣟⡵⡪⢞⡡⡫⢢⢓⡭⣞⣮⠷⡛⡤⣢⣼⣌⣪⣀⡣⣂⢔⠡⡹⢄⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢝⠩⢂⠄⡪⢀⣮⢮⡿⣿⣿⣿⣾⣦⢛⡱⢱⠌⢆⠕⢕⣜⣣⠭⣽⣿⣿⣻⣿⣿⡻⢯⡩⢂⡙⣯⢖⠌⡆⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⡃⢒⢔⢔⠴⢂⠑⡾⣼⣿⣿⣾⢯⡻⡇⢌⢕⠡⡘⡎⢆⢪⡺⡢⣿⣫⡿⣿⢿⡳⠙⡂⢎⢊⡺⣵⢟⡼⡂⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⢀⠑⠥⡻⡆⠢⡊⡐⠨⠱⢜⡉⢑⡡⠟⢙⣔⠣⡊⢌⠩⢒⠅⡯⣺⢇⢍⢂⢄⣒⢈⡆⣰⣖⡭⡾⡶⣟⡼⡭⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢕⢬⡛⢮⡤⣱⡘⡧⡑⡢⡩⢉⢄⠜⡸⢕⡕⢕⠑⡜⢅⢛⢆⣿⡘⣕⠢⠥⣤⢳⡼⣵⠳⣽⣻⣝⣿⣻⠎⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠢⣹⢎⠷⣮⡭⣙⡖⣳⡱⣍⠲⠠⠪⡊⢞⢎⠢⠁⢊⠄⡑⠘⢮⡳⣈⠣⣚⢐⠫⣾⣏⣿⢿⣮⣟⣾⠉⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠐⢌⢯⡻⡰⣯⣋⣝⣥⡝⢇⣨⠣⡑⢌⠮⢇⣐⣆⡐⣴⢨⣦⣵⢋⢖⢓⣑⠯⡞⢶⣾⣻⢟⣷⣟⡎⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠘⠰⡧⣽⣽⡻⣽⡳⢃⠞⣈⠦⢹⠔⡢⢅⣙⠋⢬⡏⡺⣙⡭⡺⣕⣕⣑⣋⡚⢥⣬⣵⣯⡵⠋⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢜⣮⢼⣛⡶⣖⡂⣊⠴⢤⠗⢖⢙⣌⣳⢿⢢⣕⢪⡟⣾⢵⣬⣧⣩⣙⡌⢧⣻⢯⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠫⡮⣛⣺⣭⡗⣨⣂⡵⢳⡛⣭⣫⡮⣳⣵⡻⣼⣳⢵⢯⣶⣵⣳⡽⡻⡽⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢯⣾⣛⢎⣵⡽⡷⣻⣟⡶⣿⣻⡵⣟⣽⣟⣿⡻⣞⢧⣳⠻⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠉⠛⠝⠮⢏⢶⢧⣻⢝⣮⢟⣾⡳⣽⡯⡻⠙⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠉⠊⠋⠊⠙⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
 console.log('%c' + khaos_img, "color:black; font-size: 12px; font-family: monospace; font-weight: bold;");
